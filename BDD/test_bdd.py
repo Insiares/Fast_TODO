b37b08d2dd4fa9279a0af69f5346996713e7969b
FabienS2A <fabien.soulavie@gmail.com>
2024-06-26 16:52:58 +0200

    delete_cred

--- BDD/test_bdd.py
+++ BDD/test_bdd.py
@@ -3,15 +3,11 @@ import supabase
 from supabase import create_client, Client
 import datetime
 
-url = "https://vbzkijbotwtkoyzufhik.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZiemtpamJvdHd0a295enVmaGlrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0MDI4MDMsImV4cCI6MjAzNDk3ODgwM30.38B1QxTfajCcBXyDbr-8FxyBjBmT1Gukt1CgfI2cWaY"
+url = DB_URL
+key = PWD_DB
 supabase = create_client(url, key)
 
-response = supabase.auth.sign_in_with_password(
-    credentials={"email": "fabien.soulavie@gmail.com", "password": "test"}
-)
 
-supabase.postgrest.auth(supabase.auth.get_session().access_token)
 
 response = (
     supabase.table("users")
