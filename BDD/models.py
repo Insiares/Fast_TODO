2ab76572712e93164a3662a93e79e28eb70dbeea
FabienS2A <fabien.soulavie@gmail.com>
2024-06-27 12:21:16 +0200

    comment_model.py_functions

--- BDD/models.py
+++ BDD/models.py
@@ -22,6 +22,18 @@ def add_args(func):
 
 @add_args
 def insert_db(tab, data, supabase=None):
+    """
+    Insert data into desired table
+
+    Parameters:
+    tab (string): name of the table to insert in.
+    data (dict): a dict with the values to insert.
+        for users table :{"username": "<name>", "password": "<password>"}
+        for tasks table :{"title": "<name>", "description": "<name>", 'due_date':"YYYY-MM-DD", "completed": "TRUE/FALSE", "user_id":<int>}
+
+    Returns:
+    supabase response.
+    """
     response = (
     supabase.table(tab)
     .insert(data)
@@ -32,6 +44,15 @@ def insert_db(tab, data, supabase=None):
 
 @add_args
 def fetch_tasks(user_id, supabase=None):
+    """
+    fetch tasks table
+
+    Parameters:
+    user_id (int): user id of the signed in user.
+    
+    Returns:
+    (list of dict) All the tasks from the signed in user.
+    """
     response = (supabase.table("tasks")
     .select("*")
     .eq("user_id", user_id)
@@ -41,6 +62,15 @@ def fetch_tasks(user_id, supabase=None):
 
 @add_args
 def fetch_users(user_id, supabase=None):
+    """
+    fetch users table
+
+    Parameters:
+    user_id (int): user id of the signed in user.
+    
+    Returns:
+    (dict) name and password of the signed in user.
+    """
     response = (supabase.table("users").select("*")
     .eq("user_id", user_id)
     .execute()
@@ -49,6 +79,15 @@ def fetch_users(user_id, supabase=None):
 
 @add_args
 def complete_task(task_id, supabase=None):
+    """
+    Turn the completed column of a task from FALSE to TRUE
+
+    Parameters:
+    task_id (int): the selected task id
+    
+    Returns:
+    supabase response.
+    """
     response = (
     supabase.table("tasks")
     .update({"completed": "TRUE"})
@@ -59,6 +98,18 @@ def complete_task(task_id, supabase=None):
 
 @add_args
 def update_tab(tab, col, val, id, supabase=None):
+    """
+    Replace a value in a selected table
+
+    Parameters:
+    tab (string): name of the table
+    col (string): name of the column to replace
+    val (string): new value
+    id (int): id of the row where the value to replace is
+    
+    Returns:
+    supabase response.
+    """
     response = (
     supabase.table(tab)
     .update({col : val})
@@ -70,6 +121,16 @@ def update_tab(tab, col, val, id, supabase=None):
 
 @add_args
 def delete_row(tab, id, supabase=None):
+    """
+    delete a complete row in a selected table
+
+    Parameters:
+    tab (string): name of the table
+    id (int): id of the row to delete
+    
+    Returns:
+    supabase response.
+    """
     response = (
     supabase.table(tab)
     .delete()
